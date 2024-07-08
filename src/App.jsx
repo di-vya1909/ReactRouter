5cd9f44385f8e4e90ab436314385b25a95949df7
Divya <divya@Divyas-MacBook-Air-2.local>
2024-07-08 20:30:08 +0530

    reactRouter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
